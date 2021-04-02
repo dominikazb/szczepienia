0b001f97b194dbc6923600a8497b3a5e3618ffaa
dominikazb <domi.biesiada@gmail.com>
2021-04-02 15:17:04 +0200

    Task 9 - Adding screens 8,9

--- src/app/components/question-screens/screen8/screen8.component.ts
+++ src/app/components/question-screens/screen8/screen8.component.ts
@@ -1,16 +1,53 @@
 import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {ProcessVariablesService} from '../../shared/services/process-variables.service';
+import {ResultsService} from '../../shared/results/results.service';
+import {Router} from '@angular/router';
 
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'screen8',
-  templateUrl: './screen8.component.html',
-  styleUrls: ['./screen8.component.css']
+  templateUrl: './screen8.component.html'
 })
 export class Screen8Component implements OnInit {
 
-  constructor() { }
+  public answers: string[] = [];
+  public question8 = '';
+  public nextStepButtonText = '';
+  // @ts-ignore
+  public questionnaireForm: FormGroup;
+
+  constructor(private processVariablesService: ProcessVariablesService,
+              private resultsService: ResultsService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    // @ts-ignore
+    this.question8 = this.processVariablesService.questions[7].question8;
+    this.getPossibleAnswers();
+    this.nextStepButtonText = this.processVariablesService.nextStepButtonText;
+    this.buildForm();
   }
 
+  getPossibleAnswers(): void {
+    // @ts-ignore
+    this.processVariablesService.questions[7].answers.forEach(
+      (possibleAnswer: { answer: string; }) => this.answers.push(possibleAnswer.answer)
+    );
+  }
+
+  private buildForm(): void {
+    this.questionnaireForm = new FormGroup({
+      yesNoAnswer: new FormControl(this.resultsService.results.answer8, Validators.required)
+    });
+  }
+
+  public onSubmit(): void {
+    this.resultsService.results.setAnswer8(this.questionnaireForm.value.yesNoAnswer);
+    this.router.navigate(['/question9']).then(() => {});
+  }
+
+  public goBack(): void {
+    this.router.navigate(['/question7']).then(() => {});
+  }
 }
--- src/app/components/question-screens/screen9/screen9.component.ts
+++ src/app/components/question-screens/screen9/screen9.component.ts
@@ -1,16 +1,53 @@
 import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {ProcessVariablesService} from '../../shared/services/process-variables.service';
+import {ResultsService} from '../../shared/results/results.service';
+import {Router} from '@angular/router';
 
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'screen9',
-  templateUrl: './screen9.component.html',
-  styleUrls: ['./screen9.component.css']
+  templateUrl: './screen9.component.html'
 })
 export class Screen9Component implements OnInit {
 
-  constructor() { }
+  public answers: string[] = [];
+  public question9 = '';
+  public nextStepButtonText = '';
+  // @ts-ignore
+  public questionnaireForm: FormGroup;
+
+  constructor(private processVariablesService: ProcessVariablesService,
+              private resultsService: ResultsService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    // @ts-ignore
+    this.question9 = this.processVariablesService.questions[8].question9;
+    this.getPossibleAnswers();
+    this.nextStepButtonText = this.processVariablesService.nextStepButtonText;
+    this.buildForm();
   }
 
+  getPossibleAnswers(): void {
+    // @ts-ignore
+    this.processVariablesService.questions[8].answers.forEach(
+      (possibleAnswer: { answer: string; }) => this.answers.push(possibleAnswer.answer)
+    );
+  }
+
+  private buildForm(): void {
+    this.questionnaireForm = new FormGroup({
+      yesNoAnswer: new FormControl(this.resultsService.results.answer9, Validators.required)
+    });
+  }
+
+  public onSubmit(): void {
+    this.resultsService.results.setAnswer9(this.questionnaireForm.value.yesNoAnswer);
+    this.router.navigate(['/question10']).then(() => {});
+  }
+
+  public goBack(): void {
+    this.router.navigate(['/question8']).then(() => {});
+  }
 }
